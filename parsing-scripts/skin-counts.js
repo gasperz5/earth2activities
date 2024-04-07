a41b284de1ae5e5fcbecaff728fbe74511f2a174
Gašper Žitko <gasper.zitko@gmail.com>
2024-04-07 19:11:36 +0200

    New Cyber

--- parsing-scripts/skin-counts.js
+++ parsing-scripts/skin-counts.js
@@ -1,5 +1,5 @@
 // @name         Skin Counts
-// @version      0.1.3
+// @version      0.1.4
 // @description  This script will read the files and print the counts of skins bought and gift codes bought
 // @author       GasperZ5 -- gasperz (Discord) -- gasper (7.5% code for E2)
 // @support      https://www.buymeacoffee.com/gasper
@@ -20,8 +20,9 @@ if (argv.length < 3) {
     meta = { startTimestamp: new Date('2024-03-20T17:35:00.000Z'), files: ['2024-03-20.log', '2024-03-21.log'], drop: 'E2 GDC 2024' }
     meta = { startTimestamp: new Date('2024-03-24T06:30:00.000Z'), files: ['2024-03-24.log', '2024-03-25.log'], drop: 'Jolly Roger' }
     meta = { startTimestamp: new Date('2024-03-31T04:10:00.000Z'), files: ['2024-03-31.log', '2024-04-01.log'], drop: 'Heartbeat Hopper' }
+    meta = { startTimestamp: new Date('2024-04-01T09:05:00.000Z'), files: ['2024-04-01.log', '2024-04-02.log'], drop: 'WP Skin' }
     */
-   meta = { startTimestamp: new Date('2024-04-01T09:05:00.000Z'), files: ['2024-04-01.log', '2024-04-02.log'], drop: 'WP Skin' }
+   meta = { startTimestamp: new Date('2024-04-07T17:05:00.000Z'), files: ['2024-04-07.log', '2024-04-08.log'], drop: 'Cyber Ring' }
 
 
 } else {
