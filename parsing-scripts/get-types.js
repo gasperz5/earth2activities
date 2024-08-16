32a7a4fcb8a3ffb8201d91ae51a1ab813a0ecd67
Gašper Žitko <gasper.zitko@gmail.com>
2024-08-16 14:24:07 +0200

    update

--- parsing-scripts/get-types.js
+++ parsing-scripts/get-types.js
@@ -10,6 +10,7 @@ if (argv.length < 3) {
     console.log('Usage: node get-types.js <file-name> [file-name] ...');
     process.exit(1);
 }
+let types = {};
 
 for (let i = 2; i < argv.length; i++) {
     console.log(`\nFile: ${argv[i]}`);
@@ -17,18 +18,22 @@ for (let i = 2; i < argv.length; i++) {
     const file = fs.readFileSync(fileName, 'utf8');
     const lines = file.split('\r\n');
     let objects = [];
-    let types = {};
 
     for (let index = 0; index < lines.length - 1; index++) {
-        const element = JSON.parse(lines[index]);
-        objects.push(element);
-        if (!types[element.data.activity_type]) {
-            types[element.data.activity_type] = { count: 0 };
+        try{
+            element = JSON.parse(lines[index]);
+            objects.push(element);
+            if (!types[element.data.activity_type]) {
+                types[element.data.activity_type] = { count: 0 };
+            }
+            types[element.data.activity_type].count++;
+        }catch (e) {
+
         }
-        types[element.data.activity_type].count++;
     }
 
-    for (const key in types) {
-        console.log(key, '-', types[key].count);
-    }
+}
+
+for (const key in types) {
+    console.log(key, '-', types[key].count);
 }
